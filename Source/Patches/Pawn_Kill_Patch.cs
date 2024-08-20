588af68f02470344a4a01627b38dd6d3a23d6f70
Bioscrimson <yaialaadk4blade@gmail.com>
2024-08-20 17:07:34 +0200

    Add files via upload

--- Source/Patches/Pawn_Kill_Patch.cs
+++ Source/Patches/Pawn_Kill_Patch.cs
@@ -1,25 +1,21 @@
 ﻿using HarmonyLib;
 using Verse;
-using System.Linq;
 
 namespace Dynamic_Wildlife.Patches
 {
     [HarmonyPatch(typeof(Pawn), "Kill")]
     public static class Pawn_Kill_Patch
     {
-        static void Postfix(Pawn __instance, DamageInfo? dinfo, Hediff exactCulprit)
+        static bool Prefix(Pawn __instance, ref DamageInfo? dinfo, ref Hediff exactCulprit)
         {
-            // Get the map from the pawn
-            var map = __instance.Map ?? Find.Maps.FirstOrDefault(m => m.mapPawns.AllPawnsSpawned.Any(p => p == __instance));
+            // Check if the map is available before the pawn is killed
+            var map = __instance.Map ?? __instance.prevMap;
 
             if (map == null)
             {
                 Log.Warning($"Unable to find the map for pawn: {__instance.LabelShort}, Kind: {__instance.kindDef.defName}");
-                return; // Exit early if no map is available
+                return true; // Allow the original Kill method to proceed
             }
-
-            // Log message to ensure this method is being called
-            Log.Message($"Kill Postfix called for pawn: {__instance.LabelShort}, map: {map}");
 
             // Ensure that the pawn is an animal and has died
             if (__instance.RaceProps.Animal)
@@ -35,6 +31,9 @@ namespace Dynamic_Wildlife.Patches
                     Log.Warning("DynamicWildlifeMapComponent is not present on the map.");
                 }
             }
+
+            // Allow the original Kill method to proceed
+            return true;
         }
     }
 }
